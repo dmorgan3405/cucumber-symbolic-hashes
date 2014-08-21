2b48fc22cc4e2478a341fbb4b768d3ae768c072c
DXM742 <doug.morgan@leandogsoftware.com>
2014-08-21 14:33:48 -0400

    Added spec from duplicated library, and updated gem spec

--- cucumber-symbolic-hashes.gemspec
+++ cucumber-symbolic-hashes.gemspec
@@ -5,8 +5,8 @@ Gem::Specification.new do |s|
   s.description = "Cucumber Extension"
   s.authors     = ["Doug Morgan"]
   s.email       = 'doug.morgan@leandog.com'
-  s.summary 	= ""
+  s.summary 	  = ""
   s.files       = ["lib/cucumber-symbolic-hashes.rb"]
   s.homepage    = 'https://github.com/dmorgan3405/cucumber-symbolic-hashes'
-  s.license       = 'MIT'
+  s.license     = 'MIT'
 end
--- spec/symbolic_hashes_spec.rb
+++ spec/symbolic_hashes_spec.rb
@@ -5,12 +5,25 @@ require_relative '../lib/cucumber-symbolic-hashes'
 module Cucumber
   module Ast
     describe '#symbolic_hashes' do
-      before do
-        @table = Table.new([['Foo Bar', 'Bar Two Who', 'Foo Three'], %w{1 22 333}])
+
+      it 'should symbolize single key' do
+        ast_table = Table.new([['Foo Bar'], %w{1}])
+        expect(ast_table.symbolic_hashes).to eq([{:foo_bar => '1'}])
       end
 
-      it 'should return hash with symbolized keys' do
-        @table.symbolic_hashes.should == [{:foo_bar => '1', :bar_two_who => '22', :foo_three => '333'}]
+
+      it 'should symbolize multiple keys' do
+        ast_table = Table.new([['Foo Bar', 'Bar Two Who', 'Foo Three'], %w{1 22 333}])
+        expect(ast_table.symbolic_hashes).to eq([{:foo_bar => '1', :bar_two_who => '22', :foo_three => '333'}])
+      end
+
+      #defect found 
+      it 'should be able to be accessed multiple times' do
+        ast_table = Table.new([['Foo Bar', 'Bar Two Who', 'Foo Three'], %w{1 22 333}])
+        
+        symbolized_hash = ast_table.symbolic_hashes
+
+        expect(ast_table.symbolic_hashes).to eq([{:foo_bar => '1', :bar_two_who => '22', :foo_three => '333'}])
       end
 
     end
